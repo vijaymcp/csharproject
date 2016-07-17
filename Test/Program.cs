4cc2c74cce803abcf7bbe33c26dba6dedcb40393
vijayakumar R <vijay.mcp08@gmail.com>
2016-07-17 16:53:39 +0800

    test

---
+++ Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        // code added for enum value type
+        enum Status
+        {
+            Open =1,
+            Inprogress = 2,
+            ReAssign = 3,
+            Closed = 4
+
+        }
+        static void Main(string[] args)
+        {
+            Status st = Status.Open;
+            Console.WriteLine("status is {0}", st);
+            Console.ReadLine();
+
+        }
+    }
+}
